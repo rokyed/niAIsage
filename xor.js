80b9848eaa277be9cdd5c059bdf46156fba81e89
Rokyed <rokyed@gmail.com>
2018-11-29 22:42:47 -0500

    xor

---
+++ xor.js
@@ -0,0 +1,42 @@
+const tf = require('@tensorflow/tfjs');
+
+// Load the binding:
+require('@tensorflow/tfjs-node-gpu');  // Use '@tensorflow/tfjs-node-gpu' if running with GPU.
+
+const model = tf.sequential();
+
+
+model.add(tf.layers.dense({
+	units: 8,
+	inputShape: 2,
+	activation: 'tanh'
+}));
+model.add(tf.layers.dense({
+	units: 1,
+	activation: 'sigmoid'
+}));
+model.compile({
+	optimizer: 'sgd',
+	loss: 'meanSquaredError',
+	lr: 0.6
+});
+// Creating dataset
+const xs = tf.tensor2d([
+	[0, 0],
+	[0, 1],
+	[1, 0],
+	[1, 1]
+]);
+xs.print();
+const ys = tf.tensor2d([
+	[0],
+	[1],
+	[1],
+	[0]
+]);
+ys.print();
+// Train the model
+await model.fit(xs, ys, {
+	batchSize: 1,
+	epochs: 5000
+});
